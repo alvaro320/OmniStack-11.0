5d3f0aa65d33a95060ca13f81769b166bee62f68
alvaro320 <alvaro.fma@hotmail.com>
2020-04-04 12:14:08 -0300

    front-end do app

---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncidents from './pages/NewIncidents';
+
+export default function Router(){
+    return (
+        <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Logon} />
+            <Route path="/register" component={Register} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/new" component={NewIncidents} />
+        </Switch>
+        </BrowserRouter>
+
+    );
+
+}
